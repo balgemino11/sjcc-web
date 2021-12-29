356a54622bc2b50dff9d356f9c3cbc14a250fc5c
balgemino11 <balgeminoalwin@gmail.com>
2021-12-29 14:59:16 +0800

    Updates

---
+++ precache-manifest.fb7701a92664f2691a048a593dd52007.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d733b9c03f2493654a0c6488fcb506ae",
+    "url": "/sjcc-web/index.html"
+  },
+  {
+    "revision": "cf94ce3af026ad352631",
+    "url": "/sjcc-web/static/css/3.12f17c96.chunk.css"
+  },
+  {
+    "revision": "77990c20c0db29ee7468",
+    "url": "/sjcc-web/static/css/4.52c99d85.chunk.css"
+  },
+  {
+    "revision": "20cb11a169c163c25ab3",
+    "url": "/sjcc-web/static/css/main.3adba670.chunk.css"
+  },
+  {
+    "revision": "9c35bc7a96cf285fb605",
+    "url": "/sjcc-web/static/js/2.54a43324.chunk.js"
+  },
+  {
+    "revision": "cf94ce3af026ad352631",
+    "url": "/sjcc-web/static/js/3.e23aa1e1.chunk.js"
+  },
+  {
+    "revision": "77990c20c0db29ee7468",
+    "url": "/sjcc-web/static/js/4.4b405711.chunk.js"
+  },
+  {
+    "revision": "20cb11a169c163c25ab3",
+    "url": "/sjcc-web/static/js/main.c8431312.chunk.js"
+  },
+  {
+    "revision": "817cc94de475a11cd414",
+    "url": "/sjcc-web/static/js/runtime-main.ea2f60bd.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/sjcc-web/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/sjcc-web/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/sjcc-web/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "/sjcc-web/static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "c888c28f5c4910eda61abf9eea3a512f",
+    "url": "/sjcc-web/static/media/bg-app-overlay.c888c28f.png"
+  },
+  {
+    "revision": "a94be59799de9dfbc148074a2fccbfe2",
+    "url": "/sjcc-web/static/media/bg-home-agency.a94be597.jpg"
+  },
+  {
+    "revision": "4e19aff25c28a0ac47a08236420f3491",
+    "url": "/sjcc-web/static/media/bg-home-startup.4e19aff2.jpg"
+  },
+  {
+    "revision": "2ae8f97cf93d43e09ca7446c1ae88849",
+    "url": "/sjcc-web/static/media/bg-software.2ae8f97c.png"
+  },
+  {
+    "revision": "a9fb12d39a29d48b7e231683744ecac0",
+    "url": "/sjcc-web/static/media/img-1.a9fb12d3.png"
+  },
+  {
+    "revision": "9a870656b008f0418076d9c027633f06",
+    "url": "/sjcc-web/static/media/img-2.9a870656.png"
+  },
+  {
+    "revision": "e8a04101b94fb45ff932aa11d732c831",
+    "url": "/sjcc-web/static/media/logo.e8a04101.png"
+  },
+  {
+    "revision": "7912e18717d01bfdff6efd1a88541127",
+    "url": "/sjcc-web/static/media/sjcc.7912e187.jpg"
+  }
+]);
